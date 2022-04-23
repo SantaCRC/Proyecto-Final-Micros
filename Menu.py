c568c09594d73edfd45e9cbd14d1725a453fe5b7
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-04-22 23:19:49 -0600

    Menu 0.0.1

--- Menu.py
+++ Menu.py
@@ -1,1 +1,7 @@
-import multicolor
+import Multicolor
+import numpy as np
+import threading
+
+red_lower = np.array([136, 87, 111], np.uint8)
+red_upper = np.array([180, 255, 255], np.uint8)
+Multicolor.main(red_lower,red_upper)
