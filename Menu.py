d1f83d5567aa7d89f7a9af3940efc9408d2d4baa
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-04-22 23:47:04 -0600

    menu 1.0.0

--- Menu.py
+++ Menu.py
@@ -1,7 +1,26 @@
 import Multicolor
 import numpy as np
 import threading
+import PySimpleGUI as sg
 
-red_lower = np.array([136, 87, 111], np.uint8)
-red_upper = np.array([180, 255, 255], np.uint8)
-Multicolor.main(red_lower,red_upper)
+def call_blue():
+    red_lower = np.array([136, 87, 111], np.uint8)
+    red_upper = np.array([180, 255, 255], np.uint8)
+    Multicolor.main(red_lower,red_upper)
+    
+sg.theme('Default')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('Seleccione el color a detectar')],
+            [sg.Button("Azul",key="azul"),sg.Button("Verde",key="verde"),sg.Button("Rojo",key="rojo")] ]
+
+# Create the Window
+window = sg.Window('Prueba - Proyecto Final', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    if event == "rojo":
+        call_blue()
+
+window.close()
