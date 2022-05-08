45d02fe23c1a23a6ffff3af1eccf87232f01623a
SantaCRC <alvarez.fabian@outlook.com>
2022-05-07 22:17:40 -0600

    Add

--- I2C.py
+++ I2C.py
@@ -14,17 +14,18 @@ bus = SMBus(1) # indicates /dev/ic2-1
 numb = 1
  
 print ("Enter 1 for ON or 0 for OFF")
-while numb == 1:
+# while numb == 1:
  
-	ledstate = input(">>>>   ")
-	try:
-		if ledstate == "1":
-			bus.write_byte(addr, 0x1) # switch it on
-		elif ledstate == "0":
-			bus.write_byte(addr, 0x0) # switch it on
-		else:
-			numb = 0
-	except:
-		pass
-	data=bus.read_block_data(addr, 0x0)
-	print(data)
+# 	ledstate = input(">>>>   ")
+# 	try:
+# 		if ledstate == "1":
+# 			bus.write_byte(addr, 0x1) # switch it on
+# 		elif ledstate == "0":
+# 			bus.write_byte(addr, 0x0) # switch it on
+# 		else:
+# 			numb = 0
+# 	except:
+# 		pass
+	# bus.write_byte(addr, int(ledstate)) # switch it off
+while True:
+    bus.write_byte(addr, 0x1) # switch it on
