15ab4d0316d5b0d64d1650aab34ded5ddf9d48b7
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-06-15 18:57:59 -0600

    range

--- Multicolors.py
+++ Multicolors.py
@@ -48,8 +48,8 @@ def main(red_lower, red_upper):
         # define mask
         #red_lower = np.array([136, 87, 111], np.uint8)
         #red_upper = np.array([180, 255, 255], np.uint8)
-        lower_red = np.array([0,100,50])
-        upper_red = np.array([10,255,255])
+        lower_red = np.array([0,50,20])
+        upper_red = np.array([5,255,255])
         mask0 = cv2.inRange(hsvFrame, lower_red, upper_red)
 
         # upper mask (170-180)
